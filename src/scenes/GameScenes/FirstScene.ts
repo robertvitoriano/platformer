afa6fe80c23b4da89ab50cf0352bb86cafeea206
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-07-11 23:29:14 +0900

    add background music reduced

--- src/scenes/GameScenes/FirstScene.ts
+++ src/scenes/GameScenes/FirstScene.ts
@@ -105,7 +105,7 @@ export default class First extends Phaser.Scene {
     });
 
     this.matter.world.convertTilemapLayer(ground);
-    this.sound.play("a-friagem", { loop: true, volume: 0.8 });
+    this.sound.play("a-friagem", { loop: true, volume: 0.5 });
   }
 
   update(time: number, deltaTime: number) {
