50f470432456e89a3f0f666d5800a68e3b6f2448
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-03-29 17:28:47 -0300

    move menu display logic to game layer

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,6 +1,8 @@
 import Phaser from "phaser"
 import FirstScene from "./scenes/GameScenes/FirstScene/FirstScene"
 import MenuScene from "./scenes/MenuScene"
+import { useGameStateStore } from "@/store/game-state-store"
+import { GameStates } from "@/enums/game-states"
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -24,5 +26,23 @@ const game = new Phaser.Game(config)
 window.addEventListener("resize", () => {
   game.scale.resize(window.innerWidth, window.innerHeight)
 })
+
+const handleMenuDisplay = ({ code }: KeyboardEvent) => {
+  if (useGameStateStore.getState().hasStarted) {
+    if (
+      code.toLocaleLowerCase() === "escape" &&
+      useGameStateStore.getState().state !== GameStates.PAUSED
+    ) {
+      useGameStateStore.getState().setState(GameStates.PAUSED)
+    } else if (
+      code.toLocaleLowerCase() === "escape" &&
+      useGameStateStore.getState().state === GameStates.PAUSED
+    ) {
+      useGameStateStore.getState().setState(GameStates.PLAYING)
+    }
+  }
+}
+
+window.document.addEventListener("keydown", handleMenuDisplay)
 
 export default game
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,51 +1,17 @@
 import React, { useEffect } from "react";
 import { useWebsocketStore } from "../store/websocket-store";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "./components/ui/form";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+
 import { useGameStateStore } from "@/store/game-state-store";
 import { GameStates } from "@/enums/game-states";
 import { LoginForm } from "./components/LoginForm/LoginForm";
 import { PauseMenu } from "./components/PauseMenu/PauseMenu";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
-
 function App() {
   const webSocketStore = useWebsocketStore();
   const gameStateStore = useGameStateStore();
+
   useEffect(() => {
     webSocketStore.create();
-
-    const handleMenuDisplay = ({ code }: KeyboardEvent) => {
-      if (gameStateStore.hasStarted) {
-        if (code.toLocaleLowerCase() === "escape" && gameStateStore.state !== GameStates.PAUSED) {
-          gameStateStore.setState(GameStates.PAUSED);
-        } else if (
-          code.toLocaleLowerCase() === "escape" &&
-          gameStateStore.state === GameStates.PAUSED
-        ) {
-          gameStateStore.setState(GameStates.PLAYING);
-        }
-      }
-    };
-
-    document.addEventListener("keydown", handleMenuDisplay);
-
-    return () => document.removeEventListener("keydown", handleMenuDisplay);
   }, []);
 
   return (
