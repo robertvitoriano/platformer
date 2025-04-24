bb3b011a47b580da56251b8725e96526c22e47f3
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-04-24 20:04:55 -0300

    remove content from buttons

--- src/ui/components/GameButtons/GameButtons.tsx
+++ src/ui/components/GameButtons/GameButtons.tsx
@@ -12,26 +12,20 @@ export const GameButtons = () => {
   return (
     <div className="absolute bottom-50 left-0 w-full bg-transparent flex justify-around items-center">
       <button
-        className="bg-transparent text-white h-80 w-30 rounded"
+        className="bg-transparent  h-80 w-30 rounded"
         onTouchStart={() => setCommand(PlayerMoves.LEFT)}
         onTouchEnd={() => setCommand(PlayerMoves.NONE)}
-      >
-        Left
-      </button>
+      ></button>
       <button
-        className="bg-transparent text-white rounded-full h-80 w-20"
+        className="bg-transparent  rounded-full h-80 w-20"
         onTouchStart={() => setCommand(PlayerMoves.JUMP)}
         onTouchEnd={() => setCommand(PlayerMoves.NONE)}
-      >
-        Jump
-      </button>
+      ></button>
       <button
-        className="bg-transparent text-white h-80 w-30 rounded"
+        className="bg-transparent  h-80 w-30 rounded"
         onTouchStart={() => setCommand(PlayerMoves.RIGHT)}
         onTouchEnd={() => setCommand(PlayerMoves.NONE)}
-      >
-        Right
-      </button>
+      ></button>
     </div>
   );
 };
